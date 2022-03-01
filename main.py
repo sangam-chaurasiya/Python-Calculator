66a969a1b9c9679262988384f62804f5f9ec976c
Sangam Chaurasiya <76783397+sangam-chaurasiya@users.noreply.github.com>
2022-03-01 16:29:59 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+logo = '''
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+'''
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+print(logo)
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    flag = True
+    number1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    while flag:
+        operation_symbol = input("Pick an operation: ")
+        number2 = float(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(number1, number2)
+
+        print(f"{number1} {operation_symbol} {number2} = {answer}")
+        is_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        if is_continue != 'y':
+            calculator()
+        else:
+            number1 = answer
+
+
+calculator()
